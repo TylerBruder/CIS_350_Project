21b075bf61e7e8c45683b8413e3671267820563d
Tyler Bruder <tyler.bruder@bluemedora.com>
2018-07-11 11:13:40 -0400

    Added comments through out the code, and removed more unecessary methods

--- src/cis/gvsu/edu/Body.java
+++ src/cis/gvsu/edu/Body.java
@@ -3,30 +3,55 @@ package cis.gvsu.edu;
 import java.awt.Color;
 import java.awt.Graphics;
 
+
+/*
+ *Responsible for creating the snake body and drawing it
+ *
+ *@Author Tyler Bruder
+ *@Version Summer 2018
+ */
 public class Body {
 
-	private int xCoor,yCoor,width,height;
-	
-	public Body(int xCoor, int yCoor, int tileSize) {
-		this.xCoor = xCoor;
-		this.yCoor = yCoor;
-		width = tileSize;
-		height = tileSize;
-	}
-
-	public void draw(Graphics g) {
-		g.setColor(Color.GREEN);
-		g.fillRect(xCoor * width, yCoor * height, width, height);
-	}
+    /*Coordinates to place snake body and bounds to assure it stays in the frame*/
+    private int xCoor, yCoor, width, height;
 
 
-	public int getxCoor() {
-		return xCoor;
-	}
+    /*
+     *Constructor that takes the coordinates that snake will be placed at, and how big each body part is
+     *@Param xCoor
+     *@Param yCoor
+     *@Param tileSize
+     */
+    public Body(int xCoor, int yCoor, int tileSize) {
+        this.xCoor = xCoor;
+        this.yCoor = yCoor;
+        width = tileSize;
+        height = tileSize;
+    }
+
+    /*
+     *Sets the color and draws the snake body
+     *@Param g
+     */
+    public void draw(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(xCoor * width, yCoor * height, width, height);
+    }
 
 
-	public int getyCoor() {
-		return yCoor;
-	}
+    /*
+     *Getter that gets x Coordinate
+     */
+    public int getxCoor() {
+        return xCoor;
+    }
+
+
+    /*
+     *Getter that gets y Coordinate
+     */
+    public int getyCoor() {
+        return yCoor;
+    }
 
 }
--- src/cis/gvsu/edu/Main.java
+++ src/cis/gvsu/edu/Main.java
@@ -3,27 +3,37 @@ package cis.gvsu.edu;
 import javax.swing.*;
 import java.awt.*;
 
-
-
+/*
+ *Responsible for putting the game together into something usable by an end user
+ *
+ *@Author Tyler Bruder
+ *@Version Summer 2018
+ */
 public class Main {
 
-	public Main() {
-		
-		Game gamePanel = new Game();
-		JFrame frame = new JFrame();
+    /*
+     *Constructor that adds the panels to main frame, and sets border up.
+     */
+    public Main() {
 
-		frame.add(gamePanel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("SnakeGame");
-		frame.pack();
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		frame.getRootPane().setBorder(BorderFactory.createMatteBorder(3,3,3,3, Color.BLUE));
-	}
+        Game gamePanel = new Game();
+        JFrame frame = new JFrame();
 
-	public static void main(String[] args) {
+        frame.add(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("SnakeGame");
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.getRootPane().setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, Color.BLUE));
+    }
 
-		new Main();
-	}
-	
+    /*
+     *Main method that starts the game
+     */
+    public static void main(String[] args) {
+
+        new Main();
+    }
+
 }
