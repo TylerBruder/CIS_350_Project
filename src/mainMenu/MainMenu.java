84600c695f86e59d9a1fde7415a4782ea034047a
mrmattkennedy <kennemat@mail.gvsu.edu>
2018-07-12 18:58:36 -0400

    Added functionality to main menu

--- src/mainMenu/MainMenu.java
+++ src/mainMenu/MainMenu.java
@@ -10,29 +10,53 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/***********************************************************************
+ * Main menu class to organize accessing each game. Simple setup 
+ * currently.
+ * @author kennemat
+ **********************************************************************/
 public class MainMenu implements ActionListener {
+	/* JFrame to hold the panel. */
 	JFrame frame;
-	JButton button;
+	
+	/* JButton to access the Pong game. */
+	JButton breakoutButton;
+	
+	JButton snakeButton;
 	JPanel panel;
 	JLabel label = new JLabel("worked");
 	
 	public MainMenu()
 	{
-		//test
 		frame = new JFrame();
-		button = new JButton();
 		panel = new JPanel();
-		button.setPreferredSize(new Dimension(100, 30));
-		button.addActionListener(this);
-		panel.add(button);
+		
+		breakoutButton = new JButton("Breakout");
+		snakeButton = new JButton("Snake");
+		
+		breakoutButton.setPreferredSize(new Dimension(100, 30));
+		snakeButton.setPreferredSize(new Dimension(100, 30));
+		
+		breakoutButton.addActionListener(this);
+		snakeButton.addActionListener(this);
+		
+		panel.add(breakoutButton);
+		panel.add(snakeButton);
 		frame.getContentPane().add(panel);
 		frame.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
-		frame.setSize(200, 200);
+		frame.setSize(200, 120);
 		frame.setVisible(true);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		frame = new breakout.BFrame();
+		
+		if (e.getSource() == breakoutButton)
+			frame = new breakout.BFrame();
+		else if (e.getSource() == snakeButton)
+		{
+			frame.setVisible(false);
+			new cis.gvsu.edu.Main();
+		}
 		panel.add(label);
 	}
 	
